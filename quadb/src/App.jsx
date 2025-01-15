a6f8d5a9583dcd4d1a01e3fbab4333463efb2bd3
AKSHAT SHARMA <akshatgopal70@gmail.com>
2025-01-15 19:45:35 +0530

    refactor: remove StrictMode from main render and update App component formatting; add new popover component and dependencies

--- quadb/src/App.jsx
+++ quadb/src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import Home from './components/Home'
-import './index.css'
+import React from "react";
+import Home from "./components/Home";
+import "./index.css"
 
 export default function App() {
   return (
     <Home/>
-  )
+  );
 }
